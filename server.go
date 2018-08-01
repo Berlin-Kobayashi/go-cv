5dc6e97501fdd5b694e50c805aea76cd43e56c51
Daniel Schulz <daniel.schulz@ocono.de>
2018-08-01 17:11:12 +0200

    Initial commit

---
+++ server.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello World"))
+	})
+
+	http.ListenAndServe(":80", nil)
+}
