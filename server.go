c5de2661aba82180a84a2176b2e72d8f5a6780bc
Daniel Schulz <daniel.schulz@ocono.de>
2018-08-01 23:22:15 +0200

    Use gorilla websocket

--- server.go
+++ server.go
@@ -1,11 +1,48 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, User-Agent, Connection")
+	w.Header().Set("Content-Type", "application/json")
+
+	log.Print("got request:")
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello World"))
-	})
-
-	http.ListenAndServe(":80", nil)
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	log.SetFlags(0)
+	http.HandleFunc("/", echo)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
